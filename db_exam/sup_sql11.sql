137ae8799b4a880813995ad299aa1381800fe448
AlexB <lxb@lxb.info>
2021-04-18 22:30:08 +0300

    QA Prep Exam #1 HTML ex and DB update
    Here is my HTML assignment on QA Mid Term Exam and exam SQL DB

---
+++ db_exam/sup_sql11.sql
@@ -0,0 +1,241 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Apr 08, 2021 at 09:27 PM
+-- Server version: 8.0.18
+-- PHP Version: 7.1.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+02:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `sup`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `countries`
+--
+
+CREATE TABLE `countries` (
+  `id` int(11) NOT NULL,
+  `country` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4; -- COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `countries`
+--
+
+INSERT INTO `countries` (`id`, `country`) VALUES
+(1, 'Украина'),
+(2, 'Китай'),
+(3, 'Германия'),
+(4, 'США'),
+(5, 'Турция'),
+(6, 'Румыния');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `manufacturers`
+--
+
+CREATE TABLE `manufacturers` (
+  `id` int(11) NOT NULL,
+  `brand` varchar(15) NOT NULL,
+  `country_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4; -- COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `manufacturers`
+--
+
+INSERT INTO `manufacturers` (`id`, `brand`, `country_id`) VALUES
+(1, 'Lenovo', 2),
+(2, 'Apple', 4),
+(3, 'Asus', 2),
+(4, 'Dell', 2),
+(5, 'Beko', 5),
+(6, 'Arctic', 6);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;-- COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`id`, `user_id`, `product_id`) VALUES
+(1, 1, 2),
+(2, 1, 3),
+(3, 3, 4),
+(4, 5, 1),
+(5, 4, 1),
+(6, 2, 6),
+(7, 5, 4),
+(8, 3, 6),
+(9, 4, 3);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `phones`
+--
+
+CREATE TABLE `phones` (
+  `id` int(11) NOT NULL,
+  `phone_number` varchar(13) NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4; --  COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `phones`
+--
+
+INSERT INTO `phones` (`id`, `phone_number`, `user_id`) VALUES
+(1, '+380735528402', 1),
+(2, '+380675577408', 1),
+(3, '+380976671417', 3),
+(4, '+380734478412', 5);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `id` int(11) NOT NULL,
+  `model` varchar(70) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `price` double NOT NULL,
+  `m_id` int(11) NOT NULL,
+  `summa` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4; --  COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`id`, `model`, `quantity`, `price`, `m_id`, `summa`) VALUES
+(1, 'Ноутбук IdeaPad L340-15', 5, 10000, 1, 50000),
+(2, 'Ноутбук ROG Zephyrus S', 3, 15000, 3, 45000),
+(3, 'Телефон iPhone 11 Pro', 21, 25000, 2, 525000),
+(4, 'Ноутбук XPS 15', 12, 6000, 4, 72000),
+(5, 'Холодильник GNE134620X', 4, 36000, 5, 144000),
+(6, 'Ноутбук XPS 15', 2, 18000, 6, 36000);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `age` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4; --  COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `age`) VALUES
+(1, 'Александр', 'alex@gmail.com', 25),
+(2, 'Владимир', 'vlad@gmail.com', 45),
+(3, 'Любовь', 'lyba@hotmail.com', 34),
+(4, 'Андрей', 'andrea@gmail.com', 18),
+(5, 'Александр', 'alex@ukr.net', 27);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `countries`
+--
+ALTER TABLE `countries`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `manufacturers`
+--
+ALTER TABLE `manufacturers`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FK_orders_users` (`user_id`);
+
+--
+-- Indexes for table `phones`
+--
+ALTER TABLE `phones`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `phone_number` (`phone_number`),
+  ADD KEY `FK_phones_users` (`user_id`);
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FK_products_manufacturers` (`m_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `orders`
+--
+ALTER TABLE `orders`
+  ADD CONSTRAINT `FK_orders_products` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `FK_orders_users` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Constraints for table `phones`
+--
+ALTER TABLE `phones`
+  ADD CONSTRAINT `FK_phones_users` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Constraints for table `products`
+--
+ALTER TABLE `products`
+  ADD CONSTRAINT `FK_products_manufacturers` FOREIGN KEY (`m_id`) REFERENCES `manufacturers` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
